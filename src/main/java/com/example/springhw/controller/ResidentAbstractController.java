03443d861cc7306272a26a548237ad0199251353
Darius <sakas.darius@gmail.com>
2021-06-04 14:34:48 +0300

    1. Modified Config manager 2. Using RestControllers instead of Controller in child classes 3. Modified table realationships

--- src/main/java/com/example/springhw/controller/ResidentAbstractController.java
+++ src/main/java/com/example/springhw/controller/ResidentAbstractController.java
@@ -3,11 +3,8 @@ package com.example.springhw.controller;
 import com.example.springhw.model.Resident;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
